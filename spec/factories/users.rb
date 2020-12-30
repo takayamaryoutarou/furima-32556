3dba801f455863eca4bca5b8d115ad00934f0ede
takayamaryoutarou <on89takayama1126@gmail.com>
2020-12-30 11:08:23 +0900

    単体テストコードの実装

---
+++ spec/factories/users.rb
@@ -0,0 +1,19 @@
+FactoryBot.define do
+  factory :user do
+    transient do
+      person { Gimei.name }
+    end 
+
+
+
+    name                      {Faker::Name.initials(number: 2)}
+    email                     {Faker::Internet.free_email}
+    password                  {Faker::Internet.password(min_length: 6)}
+    password_confirmation     {password}
+    birthday                  {Faker::Date.backward}
+    first_name { person.first.kanji }
+    last_name { person.last.kanji }
+    first_name_kana { person.first.katakana }
+    last_name_kana { person.last.katakana }
+  end
+end
