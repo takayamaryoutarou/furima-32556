c3d326c16c836051401d6f3ccf24a5aad2f6bc19
takayamaryoutarou <on89takayama1126@gmail.com>
2021-01-09 19:32:46 +0900

    商品一覧表示、並び替え

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,7 +3,7 @@ class ItemsController < ApplicationController
   before_action :move_to_index, except: [:index, :show]
 
   def index
-    @items = Item.includes(:user)
+    @items = Item.includes(:user).order(created_at: :desc)
   end
 
   def new
