5cfec22a3c80eba26427bbda481c622e632c35f0
takayamaryoutarou <on89takayama1126@gmail.com>
2021-01-09 12:17:38 +0900

    修正　１月９日

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -6,7 +6,7 @@ FactoryBot.define do
     prefecture_id              { 1 }
     delivery_date_id           { 1 }
     delivery_charge_id         { 1 }
-    price                      { '1000' }
+    price                      { 1000 }
     product_description        { '商品説明' }
       after(:build) do |item|
         item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -19,29 +19,29 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include "Product description can't be blank"
       end
       it 'category_idが空では登録できない' do
-        @item.category_id = nil
+        @item.category_id = 0
         @item.valid?
-        expect(@item.errors.full_messages).to include 'Category is not a number'
+        expect(@item.errors.full_messages).to include 'Category must be other than 0'
       end
       it 'state_idが空では登録できない' do
-        @item.state_id = nil
+        @item.state_id = 0
         @item.valid?
-        expect(@item.errors.full_messages).to include 'State is not a number'
+        expect(@item.errors.full_messages).to include 'State must be other than 0'
       end
       it 'prefecture_idが空では登録できない' do
-        @item.prefecture_id = nil
+        @item.prefecture_id = 0
         @item.valid?
-        expect(@item.errors.full_messages).to include 'Prefecture is not a number'
+        expect(@item.errors.full_messages).to include 'Prefecture must be other than 0'
       end
       it 'delivery_date_idが空では登録できない' do
-        @item.delivery_date_id = nil
+        @item.delivery_date_id = 0
         @item.valid?
-        expect(@item.errors.full_messages).to include 'Delivery date is not a number'
+        expect(@item.errors.full_messages).to include 'Delivery date must be other than 0'
       end
       it 'delivery_charge_idが空では登録できない' do
-        @item.delivery_charge_id = nil
+        @item.delivery_charge_id = 0
         @item.valid?
-        expect(@item.errors.full_messages).to include 'Delivery charge is not a number'
+        expect(@item.errors.full_messages).to include 'Delivery charge must be other than 0'
       end
       it 'priceが空では登録できない' do
         @item.price = nil
@@ -49,12 +49,12 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include "Price can't be blank"
       end
       it '価格の範囲が300以下であれば登録できないこと' do
-        @item.price = '299'
+        @item.price = 299
         @item.valid?
         expect(@item.errors.full_messages).to include 'Price is not included in the list'
       end
       it '価格の範囲が9999999以上であれば登録できないこと' do
-        @item.price = '10000000'
+        @item.price = 10000000
         @item.valid?
         expect(@item.errors.full_messages).to include 'Price is not included in the list'
       end
@@ -72,7 +72,10 @@ RSpec.describe Item, type: :model do
     context '商品登録がうまく行く時' do
 
       it '価格の範囲が300以上、9999999以下、半角数字であれば登録できる' do
-        @item.price = '301'
+        @item.price = 301
+        expect(@item).to be_valid
+      end
+      it '全ての商品情報が正しければ、登録できる' do
         expect(@item).to be_valid
       end
     end
