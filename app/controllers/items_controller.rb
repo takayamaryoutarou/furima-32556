1993937c0766473c472d198e0122062746f7d614
takayamaryoutarou <on89takayama1126@gmail.com>
2021-01-14 16:12:54 +0900

    修正　１月１４日

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -36,8 +36,11 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    @item.destroy
+    if @item.destroy
     redirect_to root_path
+    else
+      render :new
+    end
   end
 
   private
